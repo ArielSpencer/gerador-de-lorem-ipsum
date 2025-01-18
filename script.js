fd267961e1c405bace6f119e1d7f811beae2b2e7
Ariel Spencer <arielspencer.tech@gmail.com>
2025-01-18 16:21:47 -0300

    feat: add option to generate text by number of words

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const generateBtn = document.getElementById("generateBtn");
-const paragraphsInput = document.getElementById('paragraphs');
+const wordsInput = document.getElementById('words');
 const copyBtn = document.getElementById("copyBtn");
 const output = document.getElementById("output");
 
+const loremWords = [
+  "Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit",
+  "sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore",
+  "magna", "aliqua", "ut", "enim", "ad", "minim", "veniam", "quis", "nostrud",
+  "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "ea", "commodo",
+  "consequat", "duis", "aute", "irure", "dolor", "in", "reprehenderit", "in",
+  "voluptate", "velit", "esse", "cillum", "dolore", "eu", "fugiat", "nulla",
+  "pariatur", "excepteur", "sint", "occaecat", "cupidatat", "non", "proident",
+  "sunt", "in", "culpa", "qui", "officia", "deserunt", "mollit", "anim", "id", "est", "laborum"
+];
+
 generateBtn.addEventListener("click", () => {
-  const numParagraphs = parseInt(paragraphsInput.value) || 1;
-  const loremText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  const numWords = parseInt(wordsInput.value) || 1;
 
   let result = '';
-  for (let i = 0; i < numParagraphs; i++) {
-    result += loremText + '\n\n';
+  for (let i = 0; i < numWords; i++) {
+    const randomWord = loremWords[Math.floor(Math.random() * loremWords.length)];
+    result += randomWord + (i < numWords - 1 ? ' ' : '');
   }
 
   output.innerText = result;
