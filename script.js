3f85e17c1bf4520739d1c6f65fb8731c74796664
Ariel Spencer <arielspencer.tech@gmail.com>
2025-01-19 21:50:22 -0300

    feat: expand loremWords array with additional words

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ const copyBtn = document.getElementById("copyBtn");
 const output = document.getElementById("output");
 
 const loremWords = [
-  "lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit",
-  "sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore",
-  "magna", "aliqua", "ut", "enim", "ad", "minim", "veniam", "quis", "nostrud",
-  "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "ea", "commodo",
-  "consequat", "duis", "aute", "irure", "dolor", "in", "reprehenderit", "in",
-  "voluptate", "velit", "esse", "cillum", "dolore", "eu", "fugiat", "nulla",
-  "pariatur", "excepteur", "sint", "occaecat", "cupidatat", "non", "proident",
-  "sunt", "in", "culpa", "qui", "officia", "deserunt", "mollit", "anim", "id", "est", "laborum"
+  "lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "vestibulum", "et", "quam",
+  "in", "egestas", "posuere", "suspendisse", "semper", "malesuada", "urna", "vel", "volutpat", "justo", "morbi",
+  "tincidunt", "auctor", "ante", "at", "pulvinar", "rutrum", "nec", "hac", "habitasse", "platea", "dictumst",
+  "sed", "lacus", "vitae", "tempus", "lorem", "finibus", "porta", "nulla", "facilisi", "maecenas", "vehicula",
+  "venenatis", "velit", "sagittis", "sapien", "efficitur", "at", "pellentesque", "convallis", "non", "as", "nisl",
+  "leo", "curabitur", "tortor", "porttitor", "facilisis", "congue", "sodales", "nullam", "maximus", "rhoncus", "mauris",
+  "tristique", "scelerisque", "praesent", "iaculis", "elementum", "blandit", "acumina", "lacinia", "laoreet", "blandit",
+  "interdum", "accumsan", "sem", "venenatis", "blandit", "est", "malesuada", "at", "fames", "turpis", "tincidunt", "et",
+  "ullamcorper", "duis", "eget", "cursus", "diam", "sagittis", "ullamcorper", "nulla", "interdum", "ut", "ac", "sollicitudin",
+  "aenean", "mollis", "lacus", "finibus", "imperdiet", "condimentum", "efficitur", "maximus", "ut", "feugiat", "non", "a",
+  "varius", "turpis", "adipiscing", "urna", "maximus", "mattis", "nunc", "imperdiet", "dignissim", "tempus", "placerat",
+  "facilisis", "fames", "viverra", "maximus", "feugiat", "nulla", "vestibulum", "commodo", "sollicitudin", "sed", "condimentum"
 ];
 
 const capitalizeFirstLetter = (str) => str.charAt(0).toUpperCase() + str.slice(1);
